610ad09e08b2d92c4c4c027e199db3fe075ce43f
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2022-12-16 04:11:43 -0800

    commit tree e2ee6483da980cb4b5729e166dc4d24152412296

---
+++ index.js
@@ -0,0 +1,37 @@
+// test 1
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    n++;
+    return n;
+}
+
+function decrement(n) {
+    n--;
+    return n;
+}
+
+// test 2
+
+function makeInt(n) {
+    return parseInt(n,10)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
